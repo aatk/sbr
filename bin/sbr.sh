eb1d9a50d723da7a0de1d03fab32c544a29d1305
Alex <at@btrip.ru>
2022-05-30 07:32:58 +0300

    sbr

--- bin/sbr.sh
+++ bin/sbr.sh
@@ -2,7 +2,8 @@
 
 #Если ссылка автоматически не сформировалась, можно установить её вручную командами
 #chmod ugo+x /usr/local/lib/node_modules/sbr/sbr.sh
-#ln -s /usr/local/lib/node_modules/sbr/index.js /usr/local/bin/sbr
+#ln -s /usr/local/lib/node_modules/sbr/sbr.sh /usr/local/bin/sbr
+#ln -s /private/var/www/packajes/sbr/index.js /usr/local/bin/sbr
 declare -a PARAMS_ARRAY
 
 count=1
@@ -14,4 +15,5 @@ shift
 done
 
 DIRNAME=`pwd`
-/usr/local/lib/node_modules/sbr/index.js $DIRNAME "${PARAMS_ARRAY[@]}"
+#/usr/local/lib/node_modules/sbr/index.js $DIRNAME "${PARAMS_ARRAY[@]}"
+/private/var/www/packajes/sbr/index.js $DIRNAME "${PARAMS_ARRAY[@]}"
--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 #!/usr/bin/env node
-process.argv = process.argv.slice(2);
 require('./lib/cli.js')(process);
